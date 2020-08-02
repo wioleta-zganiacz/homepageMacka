b62383a81c9fc402b92f7252366c96bbc6e02338
Wioleta Zganiacz <wioletazganiacz@gmail.com>
2020-08-02 17:04:48 +0100

    Picture added & some classes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const firstName = 'Wioleta';
-const age = 36 ;
+const firstName = 'Maciek';
+const age = 32 ;
 
 console.log(`Hej cieszę się,że zagościłeś na mojej stronie. Nazywam się ${firstName} i mam ${age} lata.`);
 
