9b194659f7cfcb20bcd1636c428259865587eb98
Wioleta Zganiacz <wioletazganiacz@gmail.com>
2020-08-14 10:02:42 +0100

    adding classes

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const firstName = 'Maciek';
-const age = 32 ;
+const firstName = "Maciek";
+const age = 32;
 
-console.log(`Hej cieszę się,że zagościłeś na mojej stronie. Nazywam się ${firstName} i mam ${age} lata.`);
-
+console.log(
+  `Hej cieszę się,że zagościłeś na mojej stronie. Nazywam się ${firstName} i mam ${age} lata.`
+);
