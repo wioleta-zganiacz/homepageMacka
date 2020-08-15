89459e9c3872a063c29b022bc588bd8fad24657a
Wioleta Zganiacz <wioletazganiacz@gmail.com>
2020-08-15 10:37:06 +0100

    adding class with BEM standards

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const age = 32;
 console.log(
   `Hej cieszę się,że zagościłeś na mojej stronie. Nazywam się ${firstName} i mam ${age} lata.`
 );
+
+const heading = document.querySelector('main-heading--js');
+
+console.log(heading);
