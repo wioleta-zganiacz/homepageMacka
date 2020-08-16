cf22b2c9f4db30c851afb084880cc0782a2ee6cd
Wioleta Zganiacz <wioletazganiacz@gmail.com>
2020-08-16 17:03:47 +0100

    Adding an empty paraghraph and filled it in js

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ console.log(
   `Hej cieszę się,że zagościłeś na mojej stronie. Nazywam się ${firstName} i mam ${age} lata.`
 );
 
-const heading = document.querySelector('main__heading--js');
+const heading = document.querySelector('.main__heading--js');
 
-console.log(heading);
+heading.innerHTML = `Witaj Drogi odwiedzający, nazwyam się ${firstName} i mam ${age} lata.`;
+
+const emptyParagraph = document.querySelector('.week-summary__description--js');
+
+emptyParagraph.innerHTML = `Nawet uzuepłniłem treść JSem, WOW!`;
