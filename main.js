bd38f96cbc67a4b139ce2c2d6c6a9250136ac764
Wioleta Zganiacz <wioletazganiacz@gmail.com>
2020-09-07 12:52:34 +0100

    adding js functions

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ deathStar.fire(`Rebel ship`)
 
 const humanOne = {
   name: `Maciek`,
-  age: 32,
+  age: 32 ,
   address: {
     street: `Warszawska`,
     city: `Białystok`,
@@ -61,14 +61,97 @@ const humanOne = {
 
 const humanTwo = {
   name: `Stefan`,
-  age: humanOne.age,
+  age: 35,
   address:  humanOne.address,
 };
 
-console.log(humanOne);
-console.log(humanTwo);
+//console.log(humanOne);
+//console.log(humanTwo);
 
-humanOne.age = 35;
+//humanOne.age = 35;
 
-console.log(humanOne);
-console.log(humanTwo);
+//console.log(humanOne);
+//console.log(humanTwo);
+
+
+if(humanOne.age > humanTwo.age) {
+  console.log('Human One jest starszy');
+}
+
+if(humanOne.age === humanTwo.age) {
+  console.log('Są tego samego wieku');
+}
+
+if(humanOne.age < humanTwo.age) {
+  console.log('Human Two jest starszy');
+}
+
+if(age > humanTwo.age) {
+  console.log('Human one jest starszy');
+}
+
+if(false) {
+  console.log('to sie nie wykona')
+}
+
+if(true) {
+  console.log('to sie wykona');
+}
+
+if((humanTwo.age === 35) || humanTwo.pet ) {
+  console.log('to sie wykona, bo OR musi miec tylko jedna zmienna true');
+}
+
+if(!false) {
+  console.log('to jest przyklad na wykonanie NOT!'); 
+}
+
+if(!('Java' == 'JavaScript')) {
+  console.log('JavaScript to nie to samo to Java')
+}
+
+if(humanOne.age > humanTwo.age) {
+  console.log('Human one jest starszy');
+}else{
+  console.log('Human Two jest starszy lub sa w tym samym wieku');
+}
+
+if(humanOne.age > humanTwo.age) {
+  console.log('Human One jest starszy'); 
+}else if(humanOne.age === humanTwo.age) {
+  console.log('Są równolatkami');
+}else{
+  console.log('human Two jest starszy od reszty');
+}
+
+
+if(1 > 2) {
+  console.log('Jeden jest wieksze od dwóch');
+}else if(1 < 2 ) {
+  console.log('Jeden jest mniejsze od dwóch');
+}else{
+  console.log('Równanie jest niepoprawne');
+}
+
+const myNumber = 7;
+switch(myNumber) {
+  case 7:
+console.log('jestem siódemką');
+  break;
+
+  case 9:
+    console.log('jestem dziewiątką');
+    break;
+
+    default:
+    console.log('jestem czymś innym');
+}
+
+
+(32 > 20) ? console.log('to prawda'):console.log('to nie prawda');
+
+const result = (32 > 20) ? age : false;
+console.log(result); 
+
+const result2 = ( humanOne.age > humanTwo.age) ? age : false;
+console.log(result2);
